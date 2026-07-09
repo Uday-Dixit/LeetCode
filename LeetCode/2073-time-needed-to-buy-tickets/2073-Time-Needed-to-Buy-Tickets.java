4ec451da2540ec9afa7b188edfac2a28d409c1fc
Uday-Dixit <udaydixit420@gmail.com>
2026-07-09 16:33:58 +0530

    [2073] [Time Beats: 100.00%] [Memory Beats: 47.69%] - LeetPush

--- LeetCode/2073-time-needed-to-buy-tickets/2073-Time-Needed-to-Buy-Tickets.java
+++ LeetCode/2073-time-needed-to-buy-tickets/2073-Time-Needed-to-Buy-Tickets.java
@@ -1,18 +1,13 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
-        int seconds=0;
-        Queue<Integer> q = new LinkedList<>();
+        int second=0;
         for(int i=0;i<tickets.length;i++){
-            q.add(i);
-        }
-        while(tickets[k]!=0){
-            int curr = q.remove();
-            tickets[curr]--;
-            if(tickets[curr]>0){
-                q.add(curr);
+            if(i>k){
+                second=second+Math.min(tickets[i],tickets[k]-1);
+            }else{
+                second=second+Math.min(tickets[i],tickets[k]);
             }
-            seconds++;
         }
-        return seconds;
+        return second;
     }
 }
