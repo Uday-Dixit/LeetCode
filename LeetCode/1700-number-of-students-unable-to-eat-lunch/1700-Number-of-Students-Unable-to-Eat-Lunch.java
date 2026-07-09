69501b3c0c0c9976fa93740aba404bb7a6d50a2b
Uday-Dixit <udaydixit420@gmail.com>
2026-07-09 14:28:00 +0530

    [1700] [Time Beats: 100.00%] [Memory Beats: 85.27%] - LeetPush

--- LeetCode/1700-number-of-students-unable-to-eat-lunch/1700-Number-of-Students-Unable-to-Eat-Lunch.java
+++ LeetCode/1700-number-of-students-unable-to-eat-lunch/1700-Number-of-Students-Unable-to-Eat-Lunch.java
@@ -1,25 +1,27 @@
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
         int n = students.length;
-        int sandwich = 0;
-        Queue<Integer> q = new LinkedList<>();
+        int count0=0;
+        int count1=0;
+
         for(int i=0;i<n;i++){
-            q.add(students[i]);
-        }
-        int count=0;
-        while(!q.isEmpty()){
-            if(q.peek()==sandwiches[sandwich]){
-                q.remove();
-                sandwich++;
-                count=0;
+            if(students[i]==0){
+                count0++;
             }else{
-                count++;
-                if(count>q.size()){
-                    break;
-                }
-                q.add(q.remove());
+                count1++;
             }
         }
-        return q.size();
+
+        for(int i=0;i<n;i++){
+            if(count0==0 && sandwiches[i]==0 || count1==0 && sandwiches[i]==1){
+                break;
+            }
+            if(sandwiches[i]==0){
+                count0--;
+            }else{
+                count1--;
+            }
+        }
+        return count0+count1;
     }
 }
