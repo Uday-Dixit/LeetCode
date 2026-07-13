8313d85962f277889868d41c47c01f5720933746
Uday-Dixit <udaydixit420@gmail.com>
2026-07-13 11:11:07 +0530

    [116] [Time Beats: 100.00%] [Memory Beats: 87.46%] - LeetPush

--- LeetCode/116-populating-next-right-pointers-in-each-node/116-Populating-Next-Right-Pointers-in-Each-Node.java
+++ LeetCode/116-populating-next-right-pointers-in-each-node/116-Populating-Next-Right-Pointers-in-Each-Node.java
@@ -23,22 +23,17 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
-        if(root==null){
-            return root;
+        if(root==null)return root;
+        if(root.left!=null){
+            root.left.next=root.right;
         }
-        Queue<Node> q = new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                Node curr = q.remove();
-                if(i!=size-1){
-                    curr.next=q.peek();
-                }
-                if (curr.left != null) q.add(curr.left);
-                if (curr.right != null) q.add(curr.right);
-            }
+
+        if(root.right != null && root.next!=null){
+            root.right.next=root.next.left;
         }
+
+        connect(root.left);
+        connect(root.right);
         return root;
     }
 }
